f57b4e26319c26dad5331daf6fedc4d013fa47e9
Sam Fred <samyung05@gmail.com>
2023-06-11 18:12:18 -0400

    Update Laurels.jsx

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images, data } from '../../constants';
 import './Laurels.css';
 
-const Laurels = () => (
-  <div>
-    Laurels
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
+  <div className="app__laurels_awards-card">
+    <img src={imgUrl} alt='awards' />
+    <div className="app__laurels_awards-card_content">
+      
+    </div>
   </div>
 );
 
