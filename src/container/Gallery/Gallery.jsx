fff5bf9592ccbce9f69cb44cf2f35eee2984f3bf
Sam Fred <samyung05@gmail.com>
2023-06-11 18:45:00 -0400

    Update Gallery.jsx

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import './Gallery.css';
 
 const Gallery = () => {
   const scrollRef = React.useRef(null);
