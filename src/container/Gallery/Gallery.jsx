3f4601b70c4433185784b09bfb6e0b1ab81eb250
Sam Fred <samyung05@gmail.com>
2023-06-11 18:36:30 -0400

    Update Gallery.jsx

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -20,7 +20,19 @@ const Gallery = () => {
   }
 
 
-  
+  return (
+    <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
+        <SubHeading title="Instagram" />
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
+        <p className='p__opensans' style={{ color: '#AAAAAA', marginTop: '2rem' }}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam tempore ratione vitae quae quas quibusdam asperiores odio. Dicta non vitae illo quo provident enim deserunt modi optio suscipit, quaerat consequatur.
+        </p>
+        <button type='button' className='custom__button'>View More</button>
+      </div>
+      
+    </div>
+  )
 };
 
 export default Gallery;
