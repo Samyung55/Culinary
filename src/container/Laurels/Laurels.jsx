a69e6d579037c4bbe09631006a44d07e1cdedf83
Sam Fred <samyung05@gmail.com>
2023-06-11 18:26:25 -0400

    Update Laurels.jsx

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -24,7 +24,9 @@ const Laurels = () => (
       </div>
     </div>
 
-   
+    <div className="app__wrapper_img">
+      <img src={images.laurels} alt="laurels_img" />
+    </div>
   </div>
 )
 export default Laurels;
