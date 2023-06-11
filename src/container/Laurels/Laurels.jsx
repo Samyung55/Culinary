fb598f1fccc026f024b608ad23a3c222186323a1
Sam Fred <samyung05@gmail.com>
2023-06-11 18:14:39 -0400

    Update Laurels.jsx

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -8,9 +8,12 @@ const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
   <div className="app__laurels_awards-card">
     <img src={imgUrl} alt='awards' />
     <div className="app__laurels_awards-card_content">
-      
+      <p className="p__cormorant" style={{ color: '#DCCA87' }}>{title}</p>
+      <p className="opensans">{subtitle}</p>
     </div>
   </div>
 );
+
+
 
 export default Laurels;
